2458ed78dd4c43f90267572e596a4e568ede9f1a
EDITH KARDA <Edith.Karda@msg.group>
2021-11-11 15:58:41 +0200

    homework 2

---
+++ minimal-docker-image/docker/docker/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$1" = "start-domain" ]
+then
+    echo "[Entrypoint] Run init scripts"
+    for f in ${SCRIPT_DIR}/init_* ${SCRIPT_DIR}/init.d/*; do
+          case "$f" in
+            *.sh)  echo "[Entrypoint] running $f"; . "$f" ;;
+            *)     echo "[Entrypoint] ignoring $f" ;;
+          esac
+          echo
+    done
+    echo "[Entrypoint] Run startInForeground script with arguments [$PAYARA_ARGS]"
+    exec ${SCRIPT_DIR}/startInForeground.sh $PAYARA_ARGS
+else
+	exec "$@"
+fi
